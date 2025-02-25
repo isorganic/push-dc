09c14b5b199aa6712200a1b2aab7719dc64f6912
isorganic <45404308+isorganic@users.noreply.github.com>
2025-02-25 16:17:57 +0700

    Create faucet.py

---
+++ faucet.py
@@ -0,0 +1,41 @@
+import requests
+import random
+import time
+import os
+from colorama import Fore
+
+channel_id = input("Masukkan ID channel: ")
+waktu = int(input("Set Waktu Kirim Pesan (dalam menit): "))
+
+time.sleep(1)
+print("3")
+time.sleep(1)
+print("2")
+time.sleep(1)
+print("1")
+time.sleep(1)
+
+os.system('cls' if os.name == 'nt' else 'clear')
+
+with open("pesan.txt", "r") as f:
+    words = f.readlines()
+
+with open("token.txt", "r") as f:
+    authorization = f.readline().strip()
+
+while True:
+    channel_id = channel_id.strip()
+
+    payload = {
+        'content': random.choice(words).strip()
+    }
+
+    headers = {
+        'Authorization': authorization
+    }
+
+    r = requests.post(f"https://discord.com/api/v9/channels/{channel_id}/messages", data=payload, headers=headers)
+    print(Fore.WHITE + "Sent message: ")
+    print(Fore.YELLOW + payload['content'])
+
+    time.sleep(waktu * 60)  # Mengubah detik ke menit
